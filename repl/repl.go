d07ea13970e4cd426ee97de64dee7da2f2ce7d76
kenshi kamata <kenshi.kamata@gmail.com>
2017-07-09 21:28:39 +0900

    chapter2.9 read-parse-print-loop

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,12 @@ import (
 	"io"
 
 	"github.com/knsh14/monkey/lexer"
-	"github.com/knsh14/monkey/token"
+	"github.com/knsh14/monkey/parser"
 )
 
-const PROMPT = ">> "
+const (
+	PROMPT = ">> "
+)
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +25,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String()+"\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
